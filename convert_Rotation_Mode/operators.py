87915a8ed5056a1a4da4999626201276dda46739
Loïc 'Lauloque' Dautry <loic.dautry@gmail.com>
2025-06-29 13:51:53 -0400

    code cleanup

--- convert_Rotation_Mode/operators.py
+++ convert_Rotation_Mode/operators.py
@@ -6,10 +6,10 @@ from .utils import dprint, is_pose_mode, get_fcurves, toggle_rotation_locks, jum
 
 
 class CRM_OT_convert_rotation_mode(Operator):
-    """Convert the selected pose bone's rotation mode on all keyframes"""
+    """Convert the selected pose bone's rotation mode."""
     bl_idname = "crm.convert_rotation_mode"
     bl_label = "Convert Rotation Mode"
-    bl_description = "Convert the selected bone's rotation mode on all keyframes."
+    bl_description = "Convert the selected bone's rotation mode."
     bl_options = {'UNDO', 'INTERNAL'}
 
     @classmethod
@@ -30,78 +30,78 @@ class CRM_OT_convert_rotation_mode(Operator):
         has_autokey = scene.tool_settings.use_keyframe_insert_auto
         scene.tool_settings.use_keyframe_insert_auto = True
 
-        initActive = context.object.data.bones.active
-        listBones = context.selected_pose_bones
-        startFrame = context.scene.frame_start
-        endFrame = context.scene.frame_end
-        initFrame = context.scene.frame_current
-        duration = endFrame - startFrame
-        amount = len(listBones)
+        initial_active_bone = context.object.data.bones.active
+        selected_bones = context.selected_pose_bones
+        start_frame = context.scene.frame_start
+        end_frame = context.scene.frame_end
+        initial_frame = context.scene.frame_current
+        duration = end_frame - start_frame
+        amount = len(selected_bones)
 
-        progressMax = amount * duration
-        wm.progress_begin(0, progressMax)
+        progress_max = amount * duration
+        wm.progress_begin(0, progress_max)
 
-        for currentBone in listBones:
+        for current_bone in selected_bones:
 
             bpy.ops.pose.select_all(action='DESELECT')
-            context.object.data.bones.active = currentBone.bone
-            currentBone.bone.select = True
+            context.object.data.bones.active = current_bone.bone
+            current_bone.bone.select = True
             dprint(
-                f"### Working on bone '{currentBone.bone.name}' ###"
+                f"### Working on bone '{current_bone.bone.name}' ###"
             )
             dprint(
                 f" # Target Rmode will be {CRM_Properties.targetRmode}"
             )
 
-            self.locks = []
-            self.locks.append(currentBone.lock_rotation[0])
-            self.locks.append(currentBone.lock_rotation[1])
-            self.locks.append(currentBone.lock_rotation[2])
-            self.locks.append(currentBone.lock_rotation_w)
-            self.locks.append(currentBone.lock_rotations_4d)
-            toggle_rotation_locks('OFF', currentBone)
+            # self.locks = []
+            self.locks.append(current_bone.lock_rotation[0])
+            self.locks.append(current_bone.lock_rotation[1])
+            self.locks.append(current_bone.lock_rotation[2])
+            self.locks.append(current_bone.lock_rotation_w)
+            self.locks.append(current_bone.lock_rotations_4d)
+            toggle_rotation_locks('OFF', current_bone)
             dprint(" |  # Backed up and unlocked rotations")
 
-            originalRmode = currentBone.rotation_mode
+            original_rmode = current_bone.rotation_mode
             bpy.ops.screen.frame_jump(end=False)
-            currentBone.rotation_mode = originalRmode
-            currentBone.keyframe_insert(
+            current_bone.rotation_mode = original_rmode
+            current_bone.keyframe_insert(
                 "rotation_mode",
                 frame=1,
-                group=currentBone.name
+                group=current_bone.name
             )
             cnt = 1
 
-            while context.scene.frame_current <= endFrame:
-                curFrame = context.scene.frame_current
-                dprint(f" |  # Jumped to frame {curFrame}")
-                progressCurrent = cnt * curFrame
-                wm.progress_update(progressCurrent)
+            while context.scene.frame_current <= end_frame:
+                current_frame = context.scene.frame_current
+                dprint(f" |  # Jumped to frame {current_frame}")
+                progress_current = cnt * current_frame
+                wm.progress_update(progress_current)
 
-                currentBone.rotation_mode = originalRmode
-                currentBone.keyframe_insert(
+                current_bone.rotation_mode = original_rmode
+                current_bone.keyframe_insert(
                     "rotation_mode",
-                    frame=curFrame,
-                    group=currentBone.name
+                    frame=current_frame,
+                    group=current_bone.name
                 )
                 dprint(
-                    f" |  |  # '{currentBone.name}' Rmode set to "
-                    f"{currentBone.rotation_mode}"
+                    f" |  |  # '{current_bone.name}' Rmode set to "
+                    f"{current_bone.rotation_mode}"
                 )
 
                 bpy.ops.object.copy_global_transform()
                 dprint(
-                    f" |  |  # Copied '{currentBone.name}' Global "
-                    f"Transform as {originalRmode}"
+                    f" |  |  # Copied '{current_bone.name}' Global "
+                    f"Transform as {original_rmode}"
                 )
 
-                currentBone.rotation_mode = CRM_Properties.targetRmode
-                currentBone.keyframe_insert(
+                current_bone.rotation_mode = CRM_Properties.targetRmode
+                current_bone.keyframe_insert(
                     "rotation_mode",
-                    frame=curFrame,
-                    group=currentBone.name)
+                    frame=current_frame,
+                    group=current_bone.name)
                 dprint(
-                    f" |  |  # Rmode set to {currentBone.rotation_mode}"
+                    f" |  |  # Rmode set to {current_bone.rotation_mode}"
                 )
 
                 bpy.ops.object.paste_transform(
@@ -109,8 +109,8 @@ class CRM_OT_convert_rotation_mode(Operator):
                     use_mirror=False
                 )
                 dprint(
-                    f" |  |  # Pasted '{currentBone.name}' Global Transform as"
-                    f" {currentBone.rotation_mode}"
+                    f" |  |  # Pasted '{current_bone.name}' Global Transform "
+                    f"as {current_bone.rotation_mode}"
                 )
                 for path in (
                     "rotation_axis_angle",
@@ -118,37 +118,37 @@ class CRM_OT_convert_rotation_mode(Operator):
                     "rotation_mode",
                     "rotation_quaternion",
                 ):
-                    currentBone.keyframe_insert(data_path=path)
+                    current_bone.keyframe_insert(data_path=path)
                 dprint(
-                    f" |  |  # Keyframed '{currentBone.name}' rotations"
+                    f" |  |  # Keyframed '{current_bone.name}' rotations"
                 )
 
                 jump_next_frame(context)
-                if curFrame == context.scene.frame_current:
+                if current_frame == context.scene.frame_current:
                     break
 
             if CRM_Properties.preserveLocks:
-                toggle_rotation_locks('ON', currentBone)
+                toggle_rotation_locks('ON', current_bone)
                 dprint(" |  # Reverted rotation locks")
 
             dprint(
-                f" # No more keyframes on '{currentBone.name}'.#"
+                f" # No more keyframes on '{current_bone.name}'.#"
             )
         dprint(" # No more bones to work on.")
 
         wm.progress_end()
         self.report(
             {"INFO"},
-            f"Successfully converted {len(listBones)} bone(s) to "
+            f"Successfully converted {len(selected_bones)} bone(s) to "
             f"'{CRM_Properties.targetRmode}'"
         )
 
         if CRM_Properties.jumpInitFrame:
-            context.scene.frame_current = initFrame
+            context.scene.frame_current = initial_frame
         if CRM_Properties.preserveSelection:
-            for bone in listBones:
+            for bone in selected_bones:
                 bone.bone.select = True
-            context.object.data.bones.active = initActive
+            context.object.data.bones.active = initial_active_bone
 
         scene.tool_settings.use_keyframe_insert_auto = has_autokey
         return {'FINISHED'}
